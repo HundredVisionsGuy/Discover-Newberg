fdc52af1d8488a6f08f8319ede224ecc26d55509
hundredvisionsguy <winikkc@hsd.k12.or.us>
2018-03-01 18:16:33 -0800

    Adding RetrofitArrayApi and fixing post_details and MainActivity

--- app/src/main/java/com/hundredvisions/discover/discovernewberg/MainActivity.java
+++ app/src/main/java/com/hundredvisions/discover/discovernewberg/MainActivity.java
@@ -4,9 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayoutManager mLayoutManager;
     private ArrayList<Model> list;
     private RecyclerViewAdapter adapter;
+    private String baseURL = "https://discover.hundredvisions.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,43 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
 
         list = new ArrayList<Model>();
+        /// call retrofit
+        getRetrofit();
+
         adapter = new RecyclerViewAdapter(list, MainActivity.this);
         recyclerView.setAdapter(adapter);
+    }
+    public void getRetrofit() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        RetrofitArrayApi service = retrofit.create(RetrofitArrayApi.class);
+        Call<List<WPPost>> call = service.getPostInfo();
+
+        call.enqueue(new Callback<List<WPPost>>() {
+            @Override
+            public void onResponse(Call<List<WPPost>> call, Response<List<WPPost>> response) {
+                Log.e("mainactivity [abc]", " response " + response.body());
+
+                progressBar.setVisibility(View.GONE);
+                for (int i=0; i<response.body().size(); i++) {
+                    String tempdetails = response.body().get(i).getExcerpt().getRendered().toString();
+                    tempdetails = tempdetails.replace("<p>", "");
+                    tempdetails = tempdetails.replace("</p>","");
+                    tempdetails = tempdetails.replace("[&hellip;]","");
+
+                    list.add(new Model(Model.IMAGE_TYPE, response.body().get(i).getTitle().getRendered(),
+                            tempdetails,
+                            response.body().get(i).getLinks().getWpFeaturedmedia().get(0).getHref()));
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<WPPost>> call, Throwable t) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/hundredvisions/discover/discovernewberg/RetrofitArrayApi.java
@@ -0,0 +1,15 @@
+package com.hundredvisions.discover.discovernewberg;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by winikkc on 3/1/2018.
+ */
+
+public interface RetrofitArrayApi {
+    @GET("android/wp-json/wp/v2/posts")
+    Call<List<WPPost>> getPostInfo();
+}
