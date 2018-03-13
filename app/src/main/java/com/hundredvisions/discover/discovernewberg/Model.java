ec1c00deee2776caba31668790b29f736dc29f52
hundredvisionsguy <winikkc@hsd.k12.or.us>
2018-03-12 17:14:42 -0700

    Adding ID to WP Post Model - still doesn't work.

--- app/src/main/java/com/hundredvisions/discover/discovernewberg/Model.java
+++ app/src/main/java/com/hundredvisions/discover/discovernewberg/Model.java
@@ -7,9 +7,10 @@ package com.hundredvisions.discover.discovernewberg;
 public class Model {
     public static final int IMAGE_TYPE = 1;
     public String title, subtitle, Image;
-    public int type;
+    public int id, type;
 
-    public Model(int mtype, String mtitle, String msubtitle, String image) {
+    public Model(int mid, int mtype, String mtitle, String msubtitle, String image) {
+        this.id = mid;
         this.title = mtitle;
         this.subtitle = msubtitle;
         this.type = mtype;
